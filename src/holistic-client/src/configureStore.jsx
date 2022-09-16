86413a38b68f7e5ee8e8e8e43b66f778170eed89
matehaxor03 <matehaxor03>
2022-09-17 09:41:13 +1000

    remove toolkit

--- src/holistic-client/src/configureStore.jsx
+++ src/holistic-client/src/configureStore.jsx
@@ -1,26 +1,15 @@
-/*import { applyMiddleware, compose, createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-
+import loggerMiddleware from 'redux-logger'
 import monitorReducersEnhancer from './enhancers/monitorReducer'
-import loggerMiddleware from './middleware/logger'
 import rootReducer from './reducers'
 
-export default function configureStore(preloadedState) {
-  const middlewares = [loggerMiddleware, thunkMiddleware]
-  const middlewareEnhancer = applyMiddleware(...middlewares)
+const middlewares = [loggerMiddleware, thunkMiddleware]
+const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = compose(...enhancers)
+const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+const composedEnhancers = compose(...enhancers)
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+const store = createStore(rootReducer, {}, composedEnhancers)
 
-  return store
-}
-*/
-
-import { configureStore } from '@reduxjs/toolkit'
-
-import rootReducer from './reducers'
-
-const store = configureStore({ reducer: rootReducer })
 export default store
