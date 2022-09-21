92940c18fe3a336aefb666fd47b01a22593eff91
matehaxor03 <matehaxor03>
2022-09-21 13:28:29 +1000

    fix error in valdiations

--- src/holistic_db_client/class/Common.go
+++ src/holistic_db_client/class/Common.go
@@ -18,15 +18,24 @@ func Contains(array []string, str *string, label string) []error {
 }
 
 func ArrayContainsArray(array []string, second_array []string, label string) []error {
+	var errors []error 
+	var array_found []string
+	
 	for _, array_value := range array {
 		for _, second_value := range second_array {
 			if array_value == second_value {
-				return nil
+				array_found = append(array_found, second_value)
 			}
 		}
 	}
 
-	var errors []error 
-    errors = append(errors, fmt.Errorf("%s has value '%s' expected to have value in %s", label, second_array, array))
-	return errors
+	if len(array_found) != len(second_array) {
+		errors = append(errors, fmt.Errorf("%s has value '%s' expected to have value in %s", label, second_array, array))
+	}
+
+	if len(errors) > 0 {
+		return errors
+	}
+
+	return nil
 }
