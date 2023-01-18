9863737b2aea484e304f321d37044e2feece5d7c
matehaxor03 <matehaxor03>
2023-01-18 19:21:43 +1100

    update deps

--- go.mod
+++ go.mod
@@ -4,14 +4,14 @@ go 1.19
 
 require (
 	github.com/matehaxor03/holistic_common v0.0.65
-	github.com/matehaxor03/holistic_processor v0.0.11
-	github.com/matehaxor03/holistic_queue v0.0.6
-	github.com/matehaxor03/holistic_webserver v0.0.4
+	github.com/matehaxor03/holistic_json v0.0.88
+	github.com/matehaxor03/holistic_processor v0.0.13
+	github.com/matehaxor03/holistic_queue v0.0.9
+	github.com/matehaxor03/holistic_webserver v0.0.5
 )
 
 require (
 	github.com/matehaxor03/holistic_db_client v0.0.531 // indirect
 	github.com/matehaxor03/holistic_http v0.0.17 // indirect
-	github.com/matehaxor03/holistic_json v0.0.88 // indirect
 	github.com/matehaxor03/holistic_thread_safe v0.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	webserver "github.com/matehaxor03/holistic_webserver/webserver"
 	processor "github.com/matehaxor03/holistic_processor/processor"
+	json "github.com/matehaxor03/holistic_json/json"
 	queue "github.com/matehaxor03/holistic_queue/queue"
 	common "github.com/matehaxor03/holistic_common/common"
 	"fmt"
@@ -25,12 +26,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	complete_function := queue_server.GetCompleteFunction()
-	get_next_message_function := queue_server.GetNextMessageFunction()
-	push_message_function := queue_server.GetPushBackFunction()
+	queue_complete_functions := make(map[string](*func(json.Map) []error))
+	queue_get_next_message_functions := make(map[string]((*func(string) (json.Map, []error))))
+	queue_push_back_functions :=  make(map[string](*func(json.Map) (*json.Map,[]error)))
 
+	queue_controllers := queue_server.GetControllers()
+	for queue_controller_name, queue_controller := range queue_controllers {
+		queue_complete_functions[queue_controller_name] = queue_controller.GetCompleteFunction()
+		queue_get_next_message_functions[queue_controller_name] = queue_controller.GetNextMessageFunction()
+		queue_push_back_functions[queue_controller_name] = queue_controller.GetPushBackFunction()
+	}
 
-	web_server, web_server_errors := webserver.NewWebServer(push_message_function, "5001", "server.crt", "server.key", "127.0.0.1", "5000")
+	web_server, web_server_errors := webserver.NewWebServer("5001", "server.crt", "server.key", "127.0.0.1", "5000")
 	if web_server_errors != nil {
 		errors = append(errors, web_server_errors...)	
 	} else if common.IsNil(web_server) {
@@ -42,20 +49,28 @@ func main() {
 		os.Exit(1)
 	}
 	
-	processor_server, processor_server_errors := processor.NewProcessorServer(complete_function, get_next_message_function, push_message_function, "5002", "server.crt", "server.key", "127.0.0.1", "5000")
+	processor_server, processor_server_errors := processor.NewProcessorServer("5002", "server.crt", "server.key", "127.0.0.1", "5000")
 	if processor_server_errors != nil {
 		errors = append(errors, processor_server_errors...)	
 	} else if common.IsNil(processor_server) {
 		errors = append(errors, fmt.Errorf("web_server is nil"))
-	} 
+	} else {
+		processor_server.SetQueueCompleteFunctions(queue_complete_functions)
+		processor_server.SetQueuePushBackFunctions(queue_push_back_functions)
+		processor_server.SetQueueGetNextMessageFunctions(queue_get_next_message_functions)
+	}
 
 	if len(errors) > 0 {
 		fmt.Println(errors)
 		os.Exit(1)
 	}
 
-	processor_wakeup_function := processor_server.GetWakeupProcessorFunction()
-	queue_server.SetProcessorCallbackFunction(processor_wakeup_function)
+	processor_manager_wakeup_functions := make(map[string](*func()))
+	processor_controllers := processor_server.GetControllers()
+	for processor_controller_name, processor_controller := range processor_controllers {
+		processor_manager_wakeup_functions[processor_controller_name] = processor_controller.GetWakeupProcessorManagerFunction()
+	}
+	queue_server.SetProcessorWakeUpFunctions(processor_manager_wakeup_functions)
 
 	go web_server.Start()
 	go queue_server.Start()
